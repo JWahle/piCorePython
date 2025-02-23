96c97f122308041664a963e0d42fb1f363c6b9ec
JWahle <jwahle@github.com>
2025-02-23 21:44:35 +0100

    Updated to pCP 10

--- setup-remote-control.sh
+++ setup-remote-control.sh
@@ -28,16 +28,20 @@ mkdir -p $BUILD_DIR
 
 # Installs a module from the piCorePlayer repository - if not already installed.
 # Call like this: install_if_missing module_name
-install_if_missing() {
-  if ! tce-status -i | grep -q "$1" ; then
+install_if_missing(){
+  if tce-status -u | grep -q "$1" ; then
+    pcp-load -il "$1"
+  elif ! tce-status -i | grep -q "$1" ; then
     pcp-load -wil "$1"
   fi
 }
 
 # Installs a module from the piCorePlayer repository, at least until the next reboot - if not already installed.
 # Call like this: install_temporarily_if_missing module_name
-install_temporarily_if_missing() {
-  if ! tce-status -i | grep -q "$1" ; then
+install_temporarily_if_missing(){
+  if tce-status -u | grep -q "$1" ; then
+    pcp-load -il "$1"
+  elif ! tce-status -i | grep -q "$1" ; then
     pcp-load -wil -t /tmp "$1" # Downloads to /tmp/optional and loads extensions temporarily
   fi
 }
@@ -49,10 +53,12 @@ set -v
 install_temporarily_if_missing git
 install_if_missing python3.11
 install_if_missing python3.11-evdev
-cd /usr/local/
-python3 -m venv remote-control
-sed -i 's|include-system-site-packages = false|include-system-site-packages = true|g' remote-control/pyvenv.cfg # include system packages in the environment
-source remote-control/bin/activate # activate custom python environment
+sudo mkdir -m 775 /usr/local/remote-control
+sudo chown root:staff /usr/local/remote-control
+cd /usr/local/remote-control
+python3 -m venv environment
+sed -i 's|include-system-site-packages = false|include-system-site-packages = true|g' environment/pyvenv.cfg # include system packages in the environment
+source environment/bin/activate # activate custom python environment
 python3 -m pip install --upgrade pip
 pip install git+https://github.com/HEnquist/pycamilladsp.git@${PYCDSP_VERSION}
 mkdir -p ${BUILD_DIR}/usr/local/
@@ -65,7 +71,7 @@ cd ${BUILD_DIR}/usr/local/tce.installed/
 echo "#!/bin/sh
 sudo -u tc sh -c '
 while [ ! -f /usr/local/bin/python3 ]; do sleep 1; done
-source /usr/local/remote-control/bin/activate
+source /usr/local/remote-control/environment/bin/activate
 while [ ! -f /home/tc/remote-control.py ]; do sleep 1; done
 python3 -u /home/tc/remote-control.py > /tmp/remote-control.log 2>&1 &
 ' &" > remote-control
